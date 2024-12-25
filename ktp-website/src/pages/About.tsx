8045038ad0409d76fc59ac1ed2a2c76e9c819755
Thomas Boles <thomas211738@gmail.com>
2024-12-25 02:03:19 -0500

    added all pages and routes

---
+++ ktp-website/src/pages/About.tsx
@@ -0,0 +1,11 @@
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Information about us goes here.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ ktp-website/src/pages/Brothers.tsx
@@ -0,0 +1,11 @@
+
+function Brothers() {
+  return (
+    <div>
+      <h1>Brothers Page</h1>
+      <p>All about the Brothers.</p>
+    </div>
+  );
+}
+
+export default Brothers;
