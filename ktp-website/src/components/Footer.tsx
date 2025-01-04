747fbcf525f9e402cb543a5629b3e2f3498c8c2d
victor <vpverm@bu.edu>
2025-01-03 19:58:37 -0500

    improved footer

--- ktp-website/src/components/Footer.tsx
+++ ktp-website/src/components/Footer.tsx
@@ -1,114 +1,104 @@
-// Footer.tsx
 import Icons from "./Icons";
 import downloadonappstore from "../img/Download on App Store.png";
 
 function Footer() {
-  return (
-    <footer className="bg-[#134b91] text-white py-12">
-      {/*
-        Parent container uses Tailwind's responsive grid classes:
-        - grid-cols-1 on all screens by default
-        - md:grid-cols-3 on screens ≥ 768px
-      */}
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-4 text-center items-center px-4">
-        {/* Menu Section */}
-        <div>
-          <h3 className="font-bold text-lg mb-4">Menu</h3>
-          {/*
-            For nav items, you can also switch to a stacked approach at small
-            breakpoints to avoid horizontal text overlap
-          */}
-          <ul className="flex flex-wrap justify-center gap-4 md:gap-6">
-            <li>
-              <a href="/" className="hover:underline">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="hover:underline">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="/rush" className="hover:underline">
-                Rush
-              </a>
-            </li>
-            <li>
-              <a href="/brothers" className="hover:underline">
-                Brothers
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="hover:underline">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </div>
+    const menuItems = [
+        {
+            title: "Home",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Brothers",
+            path: "/brothers",
+        },
+        {
+            title: "Rush",
+            path: "/rush",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+    ];
+    const iconItems = [
+        { link: "https://facebook.com/ktpbostonu", icon: <Icons.Facebook /> },
+        { link: "https://instagram.com/ktpbostonu", icon: <Icons.Instagram /> },
+        {
+            link: "https://www.linkedin.com/company/kappa-theta-pi-lambda-chapter/",
+            icon: <Icons.Linkedin />,
+        },
+        {
+            link: "https://github.com/ktpbu",
+            icon: <Icons.GitHub />,
+        },
+    ];
+    return (
+        <footer className="bg-ktp-appblue text-white py-8">
+            <div className="container max-w-4/5 md:max-w-[768px] mx-auto flex flex-wrap justify-around">
+                {/* Menu Section */}
+                <div className="m-4">
+                    <h3 className="font-bold text-lg mb-4">Menu</h3>
+                    <ul className="flex flex-col flex-wrap justify-between">
+                        {menuItems.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.path} className="hover:underline">
+                                    {item.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-        {/* Follow Us Section */}
-        <div>
-          <h3 className="font-bold text-lg mb-4">Follow Us</h3>
-          {/*
-            Also use `flex-wrap` if you have more icons, so they can wrap on small screens.
-          */}
-          <div className="flex flex-wrap justify-center gap-6">
-            <a
-              href="https://facebook.com/ktpbostonu"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <Icons.Facebook />
-            </a>
-            <a
-              href="https://instagram.com/ktpbostonu"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <Icons.Instagram />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/kappa-theta-pi-lambda-chapter/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <Icons.Linkedin />
-            </a>
-          </div>
-        </div>
+                {/* Follow Us Section */}
+                <div className="m-4">
+                    <h3 className="font-bold text-lg mb-4">Follow Us</h3>
+                    <div className="max-w-24 sm:max-w-none sm:space-x-4 flex flex-wrap justify-around">
+                        {iconItems.map((item, index) => (
+                            <a
+                                key={index}
+                                href={item.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="w-12 sm:w-fit mb-4 hover:text-gray-300"
+                            >
+                                {item.icon}
+                            </a>
+                        ))}
+                    </div>
+                </div>
 
-        {/* App Section */}
-        <div>
-          <h3 className="font-bold text-lg mb-4">Boston Kappa Theta Pi App</h3>
-          <div className="flex justify-center">
-            <a
-              href="https://apps.apple.com/us/app/boston-ktp/id6654894541?platform=iphone"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <img
-                src={downloadonappstore}
-                alt="App Store"
-                className="h-12 mx-auto"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
+                {/* App Section */}
+                <div className="m-4">
+                    <h3 className="font-bold text-lg text-center mb-4">
+                        Boston KTP App
+                    </h3>
+                    <div className="flex justify-center">
+                        <a
+                            href="https://apps.apple.com/us/app/boston-ktp/id6654894541?platform=iphone"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-gray-300"
+                        >
+                            <img
+                                src={downloadonappstore}
+                                alt="App Store"
+                                className="h-12"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-      <div className="mt-12 text-center text-sm border-t border-white pt-4 px-4">
-        Developed and maintained by Lambda Chapter
-      </div>
-      <div className="mt-2 text-center text-sm">
-        © Kappa Theta Pi 2025. All rights reserved.
-      </div>
-    </footer>
-  );
+            <div className="w-fit mx-auto mt-4 flex flex-col text-center">
+                <p>Developed and maintained by Lambda Chapter</p>
+                <p>© Kappa Theta Pi 2025. All rights reserved.</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- ktp-website/src/components/Icons.tsx
+++ ktp-website/src/components/Icons.tsx
@@ -1,13 +1,15 @@
 // src/components/Icons.tsx
 
-import InstagramIcon from '../assets/instagram-icon';
-import FacebookIcon from '../assets/facebook-icon';
-import LinkedinIcon from '../assets/linkedin-icon';
+import InstagramIcon from "../assets/instagram-icon";
+import FacebookIcon from "../assets/facebook-icon";
+import LinkedinIcon from "../assets/linkedin-icon";
+import GitHubIcon from "../assets/github-icon";
 
 const Icons = {
-  Instagram: InstagramIcon,
-  Facebook: FacebookIcon,
-  Linkedin: LinkedinIcon,
+    Instagram: InstagramIcon,
+    Facebook: FacebookIcon,
+    Linkedin: LinkedinIcon,
+    GitHub: GitHubIcon,
 };
 
 export default Icons;
